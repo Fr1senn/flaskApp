d1794bff1128e1d1a6d4629b79eeb11b21863e3a
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-05-22 20:26:37 +0300

    implement alert, that shows price of purchased subscription

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -3,3 +3,11 @@ $('.slider_inner').slick({
     autoplaySpeed: 2000,
     arrows: false,
 });
+
+
+
+$('.subscription_item form button:button').click(function (){
+    sub = $('#sub').val().toString().length;
+    sub_dur = $('#sub_dur').val().toString()
+    alert('Цена: ' + parseInt(sub_dur.split(',')[0].split(' ')[0]) * sub + ' грн');
+});
