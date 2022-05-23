0e8a2ad487ad6f4d3472b1504209b9fe2b03317d
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-05-23 13:12:56 +0300

    fully completed profile

--- website/profile.py
+++ website/profile.py
@@ -14,18 +14,62 @@ def home():
     if not session['logged_in']:
         return redirect(url_for('main.login'))
     user = connect_and_execute_query(f'''
-        SELECT public.user.id, first_name, last_name, email, birthday, registration_date, "value", unit
+        SELECT DISTINCT id, first_name, last_name, birthday, registration_date, unit, LAST_VALUE(value) OVER(PARTITION BY unit)
+        FROM get_user_info('baranov@gmail.com')
+    ''', user=session['email'].split('@')[0], password=session['password'])
+    return render_template('profile/profile_base.html', user=user)
+
+
+@profile.route('/purchases')
+def purchases():
+    if not session['logged_in']:
+        return redirect(url_for('main.login'))
+    user = connect_and_execute_query(f'''
+            SELECT DISTINCT id, first_name, last_name, birthday, registration_date, unit, LAST_VALUE(value) OVER(PARTITION BY unit)
+            FROM get_user_info('baranov@gmail.com')
+        ''', user=session['email'].split('@')[0], password=session['password'])
+    purchase_list = connect_and_execute_query(f'''
+           SELECT first_name, last_name, price, date, title, duration
+           FROM public.user
+           JOIN user_subscription_duration ON user_id = public.user.id
+           JOIN subscription ON subscription.id = subscription_id
+           JOIN subscription_duration ON subscription_duration.id = subscription_duration_id
+           WHERE email = '{session['email']}';
+       ''', user=session['email'].split('@')[0], password=session['password'])
+    return render_template('profile/purchases.html', user=user, purchase_list=purchase_list)
+
+
+@profile.route('/attendance')
+def attendance():
+    if not session['logged_in']:
+        return redirect(url_for('main.login'))
+    user = connect_and_execute_query(f'''
+            SELECT DISTINCT id, first_name, last_name, birthday, registration_date, unit, LAST_VALUE(value) OVER(PARTITION BY unit)
+            FROM get_user_info('baranov@gmail.com')
+        ''', user=session['email'].split('@')[0], password=session['password'])
+    attendance_list = connect_and_execute_query(f'''
+        SELECT first_name, last_name, attendance
         FROM public.user
-        JOIN user_progress ON user_id = public.user.id
+        JOIN user_training_schedule_attendance ON user_training_schedule_attendance.user_id = public.user.id
+        JOIN attendance ON attendance.id = attendance_id
+        WHERE email = '{session['email']}';
+    ''', user=session['email'].split('@')[0], password=session['password'])
+    return render_template('profile/attendance.html', user=user, attendance_list=attendance_list)
+
+
+@profile.route('/measurements')
+def measurements():
+    if not session['logged_in']:
+        return redirect(url_for('main.login'))
+    user = connect_and_execute_query(f'''
+            SELECT DISTINCT id, first_name, last_name, birthday, registration_date, unit, LAST_VALUE(value) OVER(PARTITION BY unit)
+            FROM get_user_info('baranov@gmail.com')
+        ''', user=session['email'].split('@')[0], password=session['password'])
+    measerement_list = connect_and_execute_query(f'''
+        SELECT first_name, last_name, value, unit, date
+        FROM public.user
+        JOIN user_progress ON user_progress.user_id = public.user.id
         JOIN unit ON unit.id = unit_id
         WHERE email = '{session['email']}';
     ''', user=session['email'].split('@')[0], password=session['password'])
-    purchase_history = connect_and_execute_query(f'''
-        SELECT first_name, last_name, price, date, title, duration
-        FROM public.user
-        JOIN user_subscription_duration ON user_id = public.user.id
-        JOIN subscription ON subscription.id = subscription_id
-        JOIN subscription_duration ON subscription_duration.id = subscription_duration_id
-        WHERE email = '{session['email']}';
-    ''', user=session['email'].split('@')[0], password=session['password'])
-    return render_template('profile/profile.html', user=user, purchase_history=purchase_history)
+    return render_template('profile/measurements.html', user=user, measerement_list=measerement_list)
