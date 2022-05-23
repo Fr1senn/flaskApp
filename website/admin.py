b76912262809f5365cccb40dac80d745727a21c1
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-05-23 13:14:18 +0300

    admin panel init

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, session, url_for
+
+from . import connect_and_execute_query
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/')
 def home():
-    pass
+    if session['status'] == 'клиент':
+        return redirect(url_for('main.home'))
+    return render_template('admin/admin_base.html')
